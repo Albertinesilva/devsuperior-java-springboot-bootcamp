ad85cbbb7e14e454450fec38290e419cecd826f4
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2026-03-25 22:28:21 -0300

    Patch

--- backend/src/main/java/com/albertsilva/dev/dscatalog/web/controllers/CategoryController.java
+++ backend/src/main/java/com/albertsilva/dev/dscatalog/web/controllers/CategoryController.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,7 +49,7 @@ public class CategoryController {
     return ResponseEntity.ok(categoryService.findById(id));
   }
 
-  @PutMapping(value = "/{id}")
+  @PatchMapping(value = "/{id}")
   public ResponseEntity<CategoryResponse> update(@PathVariable Long id,
       @RequestBody CategoryUpdateRequest categoryUpdateRequest) {
     CategoryResponse categoryResponse = categoryService.update(id, categoryUpdateRequest);
