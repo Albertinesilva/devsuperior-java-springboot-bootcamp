231d1142863e1fa87aadfd7f860cec56f2f10673
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2026-05-04 10:30:36 -0300

    @ManyToMany

--- backend/src/main/java/com/albertsilva/dev/dscatalog/entities/Category.java
+++ backend/src/main/java/com/albertsilva/dev/dscatalog/entities/Category.java
@@ -2,6 +2,8 @@ package com.albertsilva.dev.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
@@ -9,6 +11,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 
@@ -102,6 +105,16 @@ public class Category implements Serializable {
    */
   @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
   private Instant updatedAt;
+
+  /**
+   * Conjunto de produtos associados a esta categoria.
+   *
+   * <p>
+   * Representa a relação de muitos para muitos entre categorias e produtos.
+   * </p>
+   */
+  @ManyToMany(mappedBy = "categories")
+  private Set<Product> products = new HashSet<>();
 
   public Category() {
   }
@@ -212,6 +225,13 @@ public class Category implements Serializable {
   @PreUpdate
   public void preUpdate() {
     updatedAt = Instant.now();
+  }
+
+  /**
+   * @return conjunto de produtos associados a esta categoria
+   */
+  public Set<Product> getProducts() {
+    return products;
   }
 
   @Override
